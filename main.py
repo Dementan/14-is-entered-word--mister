5a178e17d6980663fca2e409af45b04e31a8c82b
Dementan <masav7986@gmail.com>
2023-02-17 19:06:08 +0300

    is the entered word a word "Mister"?

---
+++ main.py
@@ -0,0 +1,9 @@
+#is the entered word a word "Mister"?
+
+word1 = input("Enter any word: ")
+word2 = "Mister"
+#compare two words
+if word1 == word2:
+    print("The entered word is a word Mister")
+else:
+    print("The entered word is not a word Mister")
